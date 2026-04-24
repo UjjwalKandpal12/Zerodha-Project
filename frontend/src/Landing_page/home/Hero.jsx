7bb771b2ead1296fdb1625dd38cdde052adafc72
Ujjwal kandpal <ujjwalkandpal5@gmail.com>
2026-04-24 23:58:28 +0530

    Fix image source path in Hero component

--- frontend/src/Landing_page/home/Hero.jsx
+++ frontend/src/Landing_page/home/Hero.jsx
@@ -3,7 +3,7 @@ function Hero() {
   return (
     <div className="container p-3 mb-5">
       <div className="row text-center">
-        <img src="media/images/homehero.png" alt="Hero" className="mb-5" />
+        <img src="media/images/homeHero.png" alt="Hero" className="mb-5" />
         <h1 className="mt-5">Invest in everything</h1>
         <p>Online platform to invest in stocks,derivatives, and mutual funds</p>
         <button className="p-2 fs-5 btn btn-primary mb-5" style={{ width: "20%", margin: "0 auto" }}>Signup now</button>
