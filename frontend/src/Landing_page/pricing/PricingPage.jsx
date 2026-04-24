b055c01d0102d059eb1a3d6da1e5b566c66cc9ba
UjjwalKandpal12 <ujjwalkandpal5@gmail.com>
2026-04-24 22:14:45 +0530

    current progress saved

--- frontend/src/Landing_page/pricing/PricingPage.jsx
+++ frontend/src/Landing_page/pricing/PricingPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Brokerage from "./Brokerage";
 import Hero from "./Hero";
 import OpenAccount from "../OpenAccount";
 function PricingPage() {
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,12 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
+import HomePage from "./Landing_page/home/HomePage";
+import Signup from "./Landing_page/signup/Signup";
+import Otp from "./Landing_page/signup/Otp";
+import AboutPage from "./Landing_page/about/AboutPage";
+import ProductPage from "./Landing_page/products/ProductPage";
+import PricingPage from "./Landing_page/pricing/PricingPage";
+import SupportPage from "./Landing_page/support/SupportPage";
+import Navbar from "./Landing_page/Navbar";
+import Footer from "./Landing_page/Footer";
+import Login from "./Landing_page/signup/Login";
+import ProtectedRoute from "./Landing_page/ProtectedRoute";
 
+import { AuthProvider } from "./Landing_page/GeneralAuth";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  </React.StrictMode>,
+  <BrowserRouter>
+    <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/otp" element={<Otp />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/products" element={<ProductPage />} />
+        <Route path="/pricing" element={<PricingPage />} />
+        <Route
+          path="/support"
+          element={
+            <ProtectedRoute>
+              <SupportPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </AuthProvider>
+  </BrowserRouter>,
 );
