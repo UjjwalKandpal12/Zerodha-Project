a25d7e28afe090501d1df77682690ef8a15d8178
Ujjwal kandpal <ujjwalkandpal5@gmail.com>
2026-04-24 23:18:45 +0530

    Change API base URL to production endpoint

--- frontend/src/Landing_page/GeneralAuth.jsx
+++ frontend/src/Landing_page/GeneralAuth.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://zerodha-project-backend1.onrender.com",
 
   withCredentials: true, // IMPORTANT for cookies
 });
