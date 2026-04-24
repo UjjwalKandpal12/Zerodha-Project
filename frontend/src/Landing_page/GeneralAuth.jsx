621c9a10ae54547f9ee183a4b1db949c9d1c9a03
Ujjwal kandpal <ujjwalkandpal5@gmail.com>
2026-04-25 00:31:56 +0530

    Update GeneralAuth.jsx

--- frontend/src/Landing_page/GeneralAuth.jsx
+++ frontend/src/Landing_page/GeneralAuth.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 export const AuthContext = createContext();
 
 const API = axios.create({
-  baseURL: "https://zerodha-project-backend1.onrender.com",
+  baseURL: "https://zerodha-project-backend1.onrender.com/api",
 
   withCredentials: true, // IMPORTANT for cookies
 });
